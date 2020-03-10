0a393d8149ff1115445b7df5e45c13b558894707
Dreisteine <dreisteine262@163.com>
2020-03-10 16:54:17 +0800

    新加入目录和文件操作

--- __init__.py
+++ __init__.py
@@ -18,8 +18,8 @@ proj_dir = os.path.abspath(os.path.dirname(__file__))
 
 with open('project_struc.yml', 'r', encoding = 'utf-8') as f:
 	fp_dict = yaml.load(f, Loader = yaml.Loader)
-	
-	
+
+
 def mk_dir(root, fp_dict):
 	"""使用递归算法创建文件目录"""
 	if fp_dict is None:
@@ -28,17 +28,31 @@ def mk_dir(root, fp_dict):
 		for key in fp_dict.keys():
 			sub_fp = os.path.join(root, key)
 			sub_fp_dict = fp_dict[key]
-			# 如果要创建的目录不存在则新建
+			# 如果要创建的目录不存在则新建.
 			if key not in os.listdir(root):
 				os.mkdir(sub_fp)
 			sub_root = sub_fp
 			mk_dir(sub_root, sub_fp_dict)
 	else:
 		raise RuntimeError('Invalid file path dict type: {}.'.format(type(fp_dict)))
-	
-	
-# 初始化项目目录结构
+
+
+def mk_file(file_dir, file_name):
+	if file_name not in os.listdir(file_dir):
+		with open(os.path.join(file_dir, file_name), 'w') as f:
+			f.write('')
+
+
+# 初始化项目目录结构.
 mk_dir(proj_dir, fp_dict['root'])
+
+# 初始化.gitignore文件.
+mk_file(proj_dir, '.gitignore')
+
+# 初始化config目录文件.
+config_dir = os.path.join(proj_dir, 'config/')
+for file in ['config.yml', 'default.yml']:
+	mk_file(config_dir, file)
 
 	
 
