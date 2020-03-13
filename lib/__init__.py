a4ba723406e6959a61aa39a73d2c5e43019f174a
Dreisteine <dreisteine262@163.com>
2020-03-13 10:31:46 +0800

    1. 加入连接PGSQL数据库功能

--- lib/__init__.py
+++ lib/__init__.py
@@ -22,17 +22,4 @@ from mod.config.config_loader import config
 proj_dir, proj_cmap = config.proj_dir, config.proj_cmap
 
 
-def search_file_in_dir(dir_path, target_names_list = None):
-	"""在目录dir_path中搜索含有target_names_list名单中元素的所有文件"""
-	if target_names_list is None:
-		return []
-	else:
-		all_files_names = [p for p in os.listdir(dir_path)]
-		target_files = []
-		for name in target_names_list:
-			target_files += [p for p in all_files_names if name in p]
-		target_files = list(set(target_files))
-		return target_files
-
-
 
